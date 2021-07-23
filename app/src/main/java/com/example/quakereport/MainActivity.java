74b989b20ee5c693ee3a74b1dd0aa3747054fd00
Hrittik <hrittikgarain@gmail.com>
2021-07-23 09:29:04 +0530

    initial point

---
+++ app/src/main/java/com/example/quakereport/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.quakereport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create a list of earthquakes locations
+        ArrayList<String> earthquakes = new ArrayList<>();
+        earthquakes.add("San Francisco");
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");
+
+        ListView earthquakesListView = (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this , android.R.layout.simple_list_item_1 , earthquakes);
+        earthquakesListView.setAdapter(adapter);
+    }
+}
